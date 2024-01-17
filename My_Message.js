fba30227653ef1dc89dcb74f9ab8e69568827671
CheggenOnline <92509643+CheggenOnline@users.noreply.github.com>
2024-01-17 22:08:20 +0100

    Add files via upload

---
+++ My_Message.js
@@ -0,0 +1,9 @@
+// developed by
+// Christian Holberg Heggen (christian.heggen@columbusglobal.com)
+var My_Message = /** @class */ (function () {
+    function My_Message() {
+        alert("you have successfully accessed my code!");
+    }
+    return My_Message;
+}());
+//# sourceMappingURL=My_Message.js.map
