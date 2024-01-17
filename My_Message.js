de6f1be67e402d9807e2f94cdd8d9b0495883c35
CheggenOnline <92509643+CheggenOnline@users.noreply.github.com>
2024-01-17 23:19:44 +0100

    Update My_Message.js

--- My_Message.js
+++ My_Message.js
@@ -1,13 +1,1 @@
-// developed by
-// Christian Holberg Heggen (christian.heggen@columbusglobal.com)
-var My_Message = /** @class */ (function () {
-    function My_Message(args) {
-    }
-    My_Message.Init = function (args) {
-        new My_Message(args).run();
-    };
-    My_Message.prototype.run = function () {
-        alert("Hell Yeah!");
-    };
-    return My_Message;
-}());
+alert("Hell Yeah!");
