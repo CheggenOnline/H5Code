f68c08ffe6d19a8d623a29b1f29edf954b2ea2f7
CheggenOnline <92509643+CheggenOnline@users.noreply.github.com>
2024-01-24 12:01:07 +0100

    Update My_Message.js

--- My_Message.js
+++ My_Message.js
@@ -1,1 +1,15 @@
-    alert("HI");
+class My_Message {
+
+
+    public static Init(args: IScriptArgs): void {
+        new My_Message(args).run();
+    }
+    constructor(args: IScriptArgs) {
+        alert("Constructor");
+    }
+    public run(): void {
+
+        alert("Hell Yeah!");
+    }
+
+}
