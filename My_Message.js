861eb2c337f0c5082c3673007dd9288f8dc4505c
CheggenOnline <92509643+CheggenOnline@users.noreply.github.com>
2024-01-25 21:06:15 +0100

    Update My_Message.js

--- My_Message.js
+++ My_Message.js
@@ -1,1 +1,27 @@
-alert("Hell YEAH!!!");
+// developed by
+// Christian Holberg Heggen (christian.heggen@columbusglobal.com)
+var My_Message = /** @class */ (function () {
+    //public static Init(args: IScriptArgs): void {
+    //    new My_Message(args).run();
+    //}
+    function My_Message() {
+        //alert("Constructor");
+    }
+    //public run(): void {
+    //    alert("Hell Yeah!");
+    //}
+    My_Message.prototype.sayHI = function () {
+        alert("Say hi!");
+    };
+    return My_Message;
+}());
+//export function hi() {
+//    alert(`Hello`);
+//}
+//export function bye() {
+//    alert(`Bye`);
+//}
+//export default function () {
+//    alert("Module loaded (export default)!");
+//}
+//# sourceMappingURL=My_Message.js.map
