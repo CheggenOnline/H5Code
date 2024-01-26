3437e408df9d998e128ed902ddda9e36436d9036
CheggenOnline <92509643+CheggenOnline@users.noreply.github.com>
2024-01-26 14:41:33 +0100

    Update My_Message.js

--- My_Message.js
+++ My_Message.js
@@ -1,14 +1,3 @@
-var My_Message = /** @class */ (function () {
-    function My_Message() {
+    function test1() {
         console.log("Constructor");
     }
-    
-    My_Message.prototype.sayHI = function () {
-        console.log("Say hi!");
-        alert("Say hi!");
-    };
-    
-    return My_Message;
-}());
-
-console.log("Script loaded");
